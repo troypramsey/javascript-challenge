2f7e731e81cad20426e486ff224efffdb686b321
Troy Ramsey <troypramsey@gmail.com>
2020-10-02 19:57:49 -0700

    made initial table dynamic and default

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,26 +1,16 @@
 // REFERENCING FORM
 var form = d3.select('form');
 
-// EVENT HANDLER AND TABLE BUILDER
-buildTable = function() {
-
-    // Stops page refresh
-    d3.event.preventDefault();
-
-    // Builds filtered data from form input
-    let input = d3.select("#dateform").property("value") 
-    let filteredData = data.filter(d => d.datetime.includes(input));
-
-    // Clear previous table if it exists
-    d3.select('table').remove();
-
+// TABLE BUILDING FUNCTION
+buildTable = function(x) {
+    
     // Build striped table skeleton
     let table = d3.select('#tableSpace').append('table').attr('class', 'table table-striped')
     let thead = table.append('thead');
     let tbody = table.append('tbody');
 
     // Build header row
-    let headers = data.map(d => Object.keys(d))[0];
+    let headers = x.map(d => Object.keys(d))[0];
     let headerRow = thead.append('tr')
         .selectAll('th')
         .data(headers)
@@ -29,7 +19,7 @@ buildTable = function() {
         .text(d => d);
 
     // Build rows and columns using filtered data
-    filteredData.forEach(d => {
+    x.forEach(d => {
         var row = tbody.append('tr');
         Object.values(d).forEach(value => {
         var cell = row.append('td');
@@ -38,6 +28,26 @@ buildTable = function() {
     })
 }
 
+// BUILD INITIAL TABLE
+buildTable(data);
+
+// EVENT HANDLER REBUILDS TABLE USING FILTERED DATA
+handleFilter = function() {
+
+    // Clear previous table if it exists
+    d3.select('table').remove();
+
+    // Stops page refresh
+    d3.event.preventDefault();
+
+    // Builds filtered data from form input
+    let input = d3.select("#dateform").property("value") 
+    let filteredData = data.filter(d => d.datetime.includes(input));
+
+    // Build new filtered table
+    buildTable(filteredData)
+}
+
 
 // EVENT LISTENER FIRES WHENEVER NEW CHARACTERS ARE INSERTED INTO FORM
-form.on('keyup',buildTable);
+form.on('keyup',handleFilter);
