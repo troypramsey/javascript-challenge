3ba3e389a3eca60ae2842982280cf7caf459c34c
Troy Ramsey <troypramsey@gmail.com>
2020-10-02 18:53:19 -0700

    built handler event

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,23 +1,45 @@
-var table = d3.select('div').append('table');
-var thead = table.append('thead');
-var tbody = table.append('tbody');
+var form = d3.select('form');
 
-var headers = data.map(d => Object.keys(d))[0];
+buildTable = function() {
 
-var headerRow = thead.append('tr')
-    .selectAll('th')
-    .data(headers)
-    .enter()
-    .append('th')
-    .text(d => d);
+    d3.event.preventDefault();
 
-data.forEach(d => {
-    let row = tbody.append('tr');
-    Object.values(d).forEach(value => {
-    let cell = row.append('td');
-    cell.text(value)
+    var input = d3.select("#dateform").property("value") 
+
+    var filteredData = data.filter(d => d.datetime === input);
+
+    console.log(filteredData);
+
+    var table = d3.select('#tableSpace').append('table').attr('class', 'table table-striped');
+    var thead = table.append('thead');
+    var tbody = table.append('tbody');
+
+    var headers = data.map(d => Object.keys(d))[0];
+
+    var headerRow = thead.append('tr')
+        .selectAll('th')
+        .data(headers)
+        .enter()
+        .append('th')
+        .text(d => d);
+
+    filteredData.forEach(d => {
+        var row = tbody.append('tr');
+        Object.values(d).forEach(value => {
+        var cell = row.append('td');
+        cell.text(value)
+        })
     })
-})
+}
 
+// searchHandler = function () {
+//     d3.event.preventDefault();
 
+//     var input = d3.select("#date-form").property("value") 
+
+//     var filteredData = data.filter(d => d.datetime.slice(1, -2) === input);
+// }
+
+form.on("submit",buildTable);
+// buildTable();
 // console.log(table);
