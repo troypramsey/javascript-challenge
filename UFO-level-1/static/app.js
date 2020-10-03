6070e7ef8108dc2fd68685dd28db856b8eba6254
Troy Ramsey <troypramsey@gmail.com>
2020-10-02 19:45:46 -0700

    cleaned up commenting and finished lvl1

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,21 +1,26 @@
+// REFERENCING FORM
 var form = d3.select('form');
 
+// EVENT HANDLER AND TABLE BUILDER
 buildTable = function() {
 
+    // Stops page refresh
     d3.event.preventDefault();
 
+    // Builds filtered data from form input
     let input = d3.select("#dateform").property("value") 
-
     let filteredData = data.filter(d => d.datetime.includes(input));
 
+    // Clear previous table if it exists
     d3.select('table').remove();
 
+    // Build striped table skeleton
     let table = d3.select('#tableSpace').append('table').attr('class', 'table table-striped')
     let thead = table.append('thead');
     let tbody = table.append('tbody');
 
+    // Build header row
     let headers = data.map(d => Object.keys(d))[0];
-
     let headerRow = thead.append('tr')
         .selectAll('th')
         .data(headers)
@@ -23,6 +28,7 @@ buildTable = function() {
         .append('th')
         .text(d => d);
 
+    // Build rows and columns using filtered data
     filteredData.forEach(d => {
         var row = tbody.append('tr');
         Object.values(d).forEach(value => {
@@ -32,14 +38,6 @@ buildTable = function() {
     })
 }
 
-// searchHandler = function () {
-//     d3.event.preventDefault();
 
-//     var input = d3.select("#date-form").property("value") 
-
-//     var filteredData = data.filter(d => d.datetime.slice(1, -2) === input);
-// }
-
+// EVENT LISTENER FIRES WHENEVER NEW CHARACTERS ARE INSERTED INTO FORM
 form.on('keyup',buildTable);
-// buildTable();
-// console.log(table);
